25fa3877bf9000f1e947cbf2679346b950d1fec9
Minh Tri <dangvantri250596@gmail.com>
2017-05-20 12:41:28 +0700

    add user page, add friend, remove friend, add list friend

--- index.js
+++ index.js
@@ -21,8 +21,6 @@ var path = require('path');
 
 require('./config/passport')(passport); // pass passport for configuration
 
-
-
 // set up our express application
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
@@ -62,6 +60,8 @@ app.use(function(req,res,next){
 // routes ======================================================================
 require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
+
 // launch ======================================================================
 app.listen(port);
 console.log('The magic happens on port ' + port);
+//console.log(user);
