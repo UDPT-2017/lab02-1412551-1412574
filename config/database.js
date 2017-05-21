93e0c7ad3365a3975dedb3491e8356b89e740743
ndtich <ndtich@gmail.com>
2017-05-21 22:16:01 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,11 +1,11 @@
 // config/database.js
 module.exports = {
     'connection': {
-        'host': 'localhost',
-        'user': 'root',
-        'password': 'root',
+        'host': 'sql111.byethost6.com',
+        'user': 'b6_19872532',
+        'password': 'visaoduaanhtoi96',
         'dateStrings':'true',
     },
-	'database': 'messenger',
+	'database': 'b6_19872532_lab2',
     'users_table': 'users',
 };
